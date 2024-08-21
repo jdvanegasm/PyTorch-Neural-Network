da4652e48ef000aeb9061bb22affbad9ff7b201b
Juan Vanegas <jdvanegasm@udistrital.edu.co>
2024-08-21 16:51:32 -0500

    New Project Structure

--- src/main.py
+++ src/main.py
@@ -4,3 +4,4 @@ import torch
 T = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
 images = torchvision.datasets.MNIST('mnist_data', transform = T, download = True)
 image_loader = torch.util.data.DataLoader(images, batch_size = 128)
+# Prueba
